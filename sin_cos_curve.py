288e5cb92a08ef4e7c0a421ecac81b343a9b792c
Atharva Kadam <165936212+Atharvakdm@users.noreply.github.com>
2026-01-14 23:25:54 -0500

    Modify trig_function to return both sin and cos

--- sin_cos_curve.py
+++ sin_cos_curve.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Define the trigonometric function
 def trig_function(x):
-    return np.sin(x)
+    return np.sin(x), np.cos(x)
     # TODO: complete the function definition
     # Hint: use np.sin() to calculate the sin of x, because np.sin(x) can handle calculating sin(x) for each item in an array.
 
@@ -12,8 +12,9 @@ x = np.linspace((-2 * np.pi), (2 * np.pi), 400)
 # Generate y values using the function
 y = trig_function(x)
 
+
 # Plot the function
-plt.plot(x, y, label='f(x) = sin(x)')
+plt.plot(x, y, label='f(x) = sin(x)','g(x) = cos(x)')
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.title('Plot of the Function f(x) = sin(x)')
